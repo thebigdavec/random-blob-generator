d605a864f559f5cf0eb93fb62bbb3853001c923b
thebigdavec <daverbcollison@gmail.com>
2021-11-27 16:19:44 +0000

    Add slider functionality
    TODO: style the sliders and find a better way to use them in vertical orientation.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const topSlide = document.getElementById('top-slide')
 const bottomSlide = document.getElementById('bottom-slide')
 const leftSlide = document.getElementById('left-slide')
 const rightSlide = document.getElementById('right-slide')
+
 const makeBlobButton = document.getElementById('make-blob-button')
 const sharpnessSlider = document.getElementById('sharpness-slider')
 const sharpnessValueText = document.getElementById('sharpness-value-text')
@@ -14,6 +15,13 @@ sharpnessSlider.oninput = function () {
   sharpnessValueText.innerText = `${sharpnessSlider.value}%`
 }
 
+topSlide.addEventListener('input', updateBlob)
+bottomSlide.addEventListener('input', updateBlob)
+leftSlide.addEventListener('input', updateBlob)
+rightSlide.addEventListener('input', updateBlob)
+
+updateBlob()
+
 makeBlobButton.addEventListener('click', makeBlob)
 
 function makeBlob() {
@@ -22,10 +30,21 @@ function makeBlob() {
   const yLeft = randomSharpness(maxSharpness)
   const yRight = randomSharpness(maxSharpness)
 
-  topSlide.style.left = `${xTop}%`
-  bottomSlide.style.left = `${xBottom}%`
-  leftSlide.style.top = `${yLeft}%`
-  rightSlide.style.top = `${yRight}%`
+  console.log({ xTop, xBottom, yLeft, yRight })
+
+  topSlide.value = `${xTop}`
+  bottomSlide.value = `${xBottom}`
+  leftSlide.value = `${yLeft}`
+  rightSlide.value = `${yRight}`
+
+  updateBlob()
+}
+
+function updateBlob() {
+  const xTop = topSlide.value
+  const xBottom = bottomSlide.value
+  const yLeft = leftSlide.value
+  const yRight = rightSlide.value
 
   theBlob.style.borderRadius = `${xTop}% ${100 - xTop}% ${
     100 - xBottom
